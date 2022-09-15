8c7a84325d0bda9d48658b1d46d3a9091e8eaade
Steffen Schieler <steffen.schieler@tu-ilmenau.de>
2022-09-15 17:01:21 +0200

    added checksums and bugfixes

--- downloader.py
+++ downloader.py
@@ -3,13 +3,17 @@ from tkinter import N
 from tqdm.auto import tqdm
 import requests
 import requests.exceptions
+import tempfile
 import os
 import subprocess
 import logging
+import pdb
 
 SERVER = "https://ftp.tu-ilmenau.de"
 DIR = "/hpc-private/ems1/test1/"
-FILE = "droneshield_h15_v11_1to2.tar.bz2"
+SCENARIOS = [
+    "droneshield_h15_v11_1to2",
+]
 
 def check_if_server_is_available(url: str) -> bool:
     try:
@@ -25,18 +29,17 @@ def check_if_server_is_available(url: str) -> bool:
 
 def check_if_file_exists_on_server(url: str) -> bool:
     file_head = requests.head(url)
-    # do smth with it to verify it is correct
     if file_head is not None:
         return True
 
     return False
 
-def download_file_from_server(url: str, out_file: str):
+def download_file_from_server(url: str, out_file: str) -> bool:
     response = requests.get(url, stream=True)  # Streaming, so we can iterate over the response.
     total_size_in_bytes= int(response.headers.get('content-length', 0))
     block_size = 1024 #1 KiB
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=f"Downloading { FILE }")
-    with open(f"{out_file}.encrypted", 'wb') as file:
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=f"Downloading { url }")
+    with open(out_file, 'wb') as file:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
             file.write(data)
@@ -44,49 +47,69 @@ def download_file_from_server(url: str, out_file: str):
     progress_bar.close()
 
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-        logging.error("Something went wrong while downloading the file.")    
+        logging.error("Something went wrong while downloading the file.")
+        return False
 
-    return
+    return True
 
-def decrypt_file(in_file: str, out_file: str = None):
+def decrypt_file(in_file: str, out_file: str = None) -> bool:
     if out_file is None:
         out_file = in_file.split(".encrypted")[0]
 
     logging.info(f"Decrypting downloaded file {in_file} as {out_file}.")
-    subprocess.run(["openssl", "enc", "-d", "-aes256", "-in", in_file, "-out", out_file])
-    print("\n")
-    return
+    proc = subprocess.run(["openssl", "enc", "-d", "-aes256", "-in", in_file, "-out", out_file])
 
-def unpack_file(out_dir: str, file: str):
+    return not(bool(proc.returncode))
+
+def unpack_file(out_dir: str, file: str) -> bool:
     logging.info(f"Unpacking archive { file } to { out_dir }.")
-    subprocess.run(["tar", "--extract", "--bzip2", "--verbose", "--directory", out_dir, "--file", file])
-    print("\n")
-    return
+    proc = subprocess.run(["tar", "--extract", "--bzip2", "--strip-components", "1", "--verbose", "--directory", out_dir, "--file", file])
 
-def check_shasum(dir: str, checksum_file: str):
-    logging.info(f"Checking Shasum of file { checksum_file.split('.checksum')[0] } to verify download.")
-    subprocess.run(["shasum", "--check", checksum_file], cwd=dir)
-    print("\n")
-    return 
+    return not(bool(proc.returncode))
 
+def check_shasum(dir: str) -> bool:
+    logging.info(f"Checking Shasum using Repos `*.checksum` files to verify download.")
+    proc = subprocess.run(["shasum", "--check", "scenarios.checksum"], cwd=dir)
+
+    return not(bool(proc.returncode))
 
 def main(args):
-    url = f"{SERVER}{ DIR }{ FILE }"
+    for scenario in args.scenario:
+        url = f"{SERVER}{ DIR }{scenario}.tar.bz2"
+        repo_dir = os.getcwd()
 
-    if not check_if_server_is_available(SERVER):
-        raise ConnectionError("Unable to connect to server!")
+        if not check_if_server_is_available(SERVER):
+            raise ConnectionError("Unable to connect to server!")
+        
+        logging.info(f"Connection to Server { SERVER } established.")
+
+        if not check_if_file_exists_on_server(url):
+            raise FileNotFoundError("File not found on server!")
+        
+        logging.info(f"Found file under URL on `{ url }`.")
+
+        # context manager deletes tmpdir when finished
+        with tempfile.TemporaryDirectory() as tmpdir:
+            encrypted_file = os.path.join(tmpdir, f"{ scenario }.tar.bz2.encrypted")
+            decrypted_file = os.path.join(tmpdir, f"{ scenario }.tar.bz2")
+
+            # download file to tmpdir
+            if not download_file_from_server(url, out_file=encrypted_file):
+                raise Exception(f"Failed to download file { url } from server. This indicates network issues.")
+
+            # decrypt file in tmpdir
+            if not decrypt_file(in_file=encrypted_file, out_file=decrypted_file):
+                raise Exception(f"Failed to decrypt file. Did you enter the correct password?")
+
+            # unpack file from tmpdir to repodir
+            if not unpack_file(out_dir=repo_dir, file=decrypted_file):
+                raise Exception(f"Failed to unpack the file for unspecific reasons. Re-run script or try manually with `tar --extract --bzip2 --verbose --directory <out_dir> --file { scenario }`.")
+
+    # check shasum of file with *.checksum file from repo
+    if not check_shasum(dir=repo_dir):
+        raise Exception(f"Shasum did not match! The file does not correspond to the one specified in the Git repository.")
     
-    logging.info(f"Connection to Server { SERVER } established.")
 
-    if not check_if_file_exists_on_server(url):
-        raise FileNotFoundError("File not found on server!")
-    
-    logging.info(f"Found file { FILE } on { SERVER }.")
-
-    download_file_from_server(url, out_file=f"./{ FILE }")
-    decrypt_file(in_file=f"{FILE}.encrypted", out_file=FILE)
-    unpack_file(out_dir="./unpacked", file=FILE)
-    check_shasum(dir="./unpacked/some_crazy_measurement/", checksum_file="some_crazy_measurement_file.checksum")
 
     logging.info("All done. Exiting.")
 
@@ -97,14 +120,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s",
         "--scenario",
-        help="The scenario to download. Must be a valid scenario string from the 2021 measurement."
-    )
-    parser.add_argument(
-        "-o",
-        "--output-dir",
-        help="Specify the output directory for the downloaded files. Default is $cwd.",
-        nargs="?",
-        default=os.getcwd(),
+        help=f"The list of scenarios to download, separated by spaces. If none is given, all scenarios are downloaded.",
+        nargs="+",
+        default=SCENARIOS,
     )
     logging.basicConfig(level=logging.INFO)
     args = parser.parse_args()
